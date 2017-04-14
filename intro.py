f74dd7326e321920206aa367dae158426c58e641
gsmekens <gsmekens@deloitte.com>
2017-04-14 13:40:52 +0200

    Notepad++

--- intro.py
+++ intro.py
@@ -1,20 +1,6 @@
-from joblib import Parallel, delayed
-import multiprocessing
-import time
+import pandas as pd
+import numpy as np
 
-def myfuntion(i):
-    print(i)
-    time.sleep(2)
-    return(i)
-if __name__ == '__main__' :
-    start = time.time()
-    for i in range(10):
-        myresults = myfuntion(i)
-    print("SERIAL", time.time() - start)
-    num_cores = multiprocessing.cpu_count()
-    print("NUmber of cores used: " + str(num_cores))
-    start = time.time()
-    results = Parallel(n_jobs=num_cores)(delayed(myfuntion(i)for i in range(10))
-    print("PARALLEL", time.time() - start)
-    print(results)
-
+col1 = [i for i in range(4)]
+col2 = [float(i**2)for i in range(4) ]
+col3 = ['cat1', 'cat2', 'cat3', 'cat1']
