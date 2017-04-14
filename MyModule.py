ecfb94f18677e760c4100794792575f58877d81a
gsmekens <gsmekens@deloitte.com>
2017-04-14 11:17:10 +0200

    I added animal class to MyModule

---
+++ MyModule.py
@@ -0,0 +1,7 @@
+class Animal(object):
+    def _init_(self, startname):
+        self.name = startname
+        self.age = age
+    def description(self):
+        print("this is " + self.name)
+        print("he/she is " + str(self.age) + "years old")
